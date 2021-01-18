2611af7d75a59cacfb58f9fcd59e22ad9de4fcc4
Daria Shifrina <dariashifrina@gmail.com>
2021-01-18 13:31:06 -0600

    latest

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -7,9 +7,6 @@
 
 import rospy
 
-# msg needed for /scan.
-from sensor_msgs.msg import LaserScan
-
 # msgs needed for /cmd_vel.
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
@@ -26,10 +23,6 @@ class DrawASquare(object):
         # Start rospy node.
         rospy.init_node("walk_to_wall")
 
-        # Declare our node as a subscriber to the scan topic and
-        #   set self.process_scan as the function to be used for callback.
-        rospy.Subscriber("/scan", LaserScan, self.process_scan)
-
         # Get a publisher to the cmd_vel topic.
         self.twist_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
@@ -40,47 +33,31 @@ class DrawASquare(object):
         self.angular_speed = 0.3
         self.relative_angle = 185 * pi / 360
 
-    def process_scan(self, data):
-        # Determine closeness to wall by looking at scan data from in front of
-        #   the robot, set linear velocity based on that information, and
-        #   publish to cmd_vel.
-
-        # The ranges field is a list of 360 number where each number
-        #   corresponds to the distance to the closest obstacle from the
-        #   LiDAR at various angles. Each measurement is 1 degree apart.
-
-        # The first entry in the ranges list corresponds with what's directly
-        #   in front of the robot.
-
-        #if data.ranges[0] >= distance:
-            # Go forward if not close enough to wall.
-        firstTime = rospy.Time.now().to_sec()
-        curTime = rospy.Time.now().to_sec() - firstTime
-        self.twist.linear.x = 0.1
-        self.twist_pub.publish(self.twist)
-        while(curTime < 10):
-            curTime = rospy.Time.now().to_sec() - firstTime
-            #self.twist_pub.publish(self.twist)
-        self.twist.linear.x = 0
-        self.twist_pub.publish(self.twist)
-        self.twist.angular.z = -abs(self.angular_speed)
-        self.twist_pub.publish(self.twist)
-        current_angle = 0
-        firstTime = rospy.Time.now().to_sec()
-        while current_angle < self.relative_angle:
-            #print(current_angle)
-            curTime = rospy.Time.now().to_sec() - firstTime
-            current_angle = self.angular_speed*(curTime)
-            #self.twist_pub.publish(self.twist)
-        print("turned 90")
-        self.twist.angular.z = 0
-        self.twist_pub.publish(self.twist)
-        rospy.Rate(10).sleep()
-        # Publish msg to cmd_vel.
-        #self.twist_pub.publish(self.twist)
-
-
     def run(self):
+        print("HERE")
+        while not rospy.is_shutdown():
+            firstTime = rospy.Time.now().to_sec()
+            curTime = rospy.Time.now().to_sec() - firstTime
+            self.twist.linear.x = 0.1
+            self.twist_pub.publish(self.twist)
+            while(curTime < 10):
+                curTime = rospy.Time.now().to_sec() - firstTime
+                #self.twist_pub.publish(self.twist)
+            self.twist.linear.x = 0
+            self.twist_pub.publish(self.twist)
+            self.twist.angular.z = -abs(self.angular_speed)
+            self.twist_pub.publish(self.twist)
+            current_angle = 0
+            firstTime = rospy.Time.now().to_sec()
+            while current_angle < self.relative_angle:
+                #print(current_angle)
+                curTime = rospy.Time.now().to_sec() - firstTime
+                current_angle = self.angular_speed*(curTime)
+                #self.twist_pub.publish(self.twist)
+            print("turned 90")
+            self.twist.angular.z = 0
+            self.twist_pub.publish(self.twist)
+            rospy.Rate(10).sleep()
         # Keep the program alive.
         rospy.spin()
 
